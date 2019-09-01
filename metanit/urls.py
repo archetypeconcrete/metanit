de529144f10f06111ddf441ab717b43b4c15e9b8
Archetype <archetypeconcrete@gmail.com>
2019-09-01 22:40:22 +0300

    111232e412323e41

--- metanit/urls.py
+++ metanit/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from firstapp import views
 
+app_name = 'firstapp'
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
+    path('contacts/', views.contacts, name='contacts'),
 ]
